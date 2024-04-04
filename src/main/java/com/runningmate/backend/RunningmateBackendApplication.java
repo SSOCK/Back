8e77fbab687f7b6572f823ea70356983c1088be0
peageon <jinsoo2005@gmail.com>
2024-04-04 16:50:23 +0900

    feat: Initialize Spring Boot Project
    Using Java 21 and gradle for this project

---
+++ src/main/java/com/runningmate/backend/RunningmateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.runningmate.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningmateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningmateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/runningmate/backend/RunningmateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runningmate.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningmateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
