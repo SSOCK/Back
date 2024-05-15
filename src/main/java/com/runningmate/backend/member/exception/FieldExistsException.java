8f3c234b26a20c55d8cdd22f94d17b90c5a24606
peageon <jinsoo2005@gmail.com>
2024-05-15 23:14:50 +0900

    feat: check whether email or username already exists

---
+++ src/main/java/com/runningmate/backend/member/exception/FieldExistsException.java
@@ -0,0 +1,16 @@
+package com.runningmate.backend.member.exception;
+
+import java.util.List;
+
+public class FieldExistsException extends RuntimeException {
+    private List<String> exists;
+
+    public FieldExistsException(List<String> exists) {
+        super("Fields already exist: " + exists);
+        this.exists = exists;
+    }
+
+    public List<String> getExists() {
+        return exists;
+    }
+}
--- src/main/java/com/runningmate/backend/member/service/MemberService.java
+++ src/main/java/com/runningmate/backend/member/service/MemberService.java
@@ -3,11 +3,15 @@ package com.runningmate.backend.member.service;
 import com.runningmate.backend.member.Member;
 import com.runningmate.backend.member.MemberSignupRequest;
 import com.runningmate.backend.member.Role;
+import com.runningmate.backend.member.exception.FieldExistsException;
 import com.runningmate.backend.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.BadRequestException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,20 +20,40 @@ public class MemberService {
     private final PasswordEncoder passwordEncoder;
 
 
-    public Member signup(MemberSignupRequest memberSignupRequest) throws BadRequestException {
-        if (memberRepository.existsByUsername(memberSignupRequest.getUsername())) {
-            throw new BadRequestException("User already exists");
+    public Member signup(MemberSignupRequest memberSignupRequest) throws FieldExistsException {
+        List<String> exists = checkExistingFields(memberSignupRequest);
+
+        if (!exists.isEmpty()) {
+            throw new FieldExistsException(exists);
         }
 
-        Member member = Member.builder()
-                .email(memberSignupRequest.getEmail())
-                .name(memberSignupRequest.getName())
-                .username(memberSignupRequest.getUsername())
-                .password(passwordEncoder.encode(memberSignupRequest.getPassword()))
-                .role(Role.USER)
-                .build();
+        Member member = createMember(memberSignupRequest);
 
         memberRepository.save(member);
         return member;
     }
+
+    private List<String> checkExistingFields(MemberSignupRequest request) {
+        List<String> exists = new ArrayList<>();
+
+        if (memberRepository.existsByEmail(request.getEmail())) {
+            exists.add("email");
+        }
+        if (memberRepository.existsByUsername(request.getUsername())) {
+            exists.add("username");
+        }
+
+        return exists;
+    }
+
+    private Member createMember(MemberSignupRequest request) {
+        return Member.builder()
+                .email(request.getEmail())
+                .name(request.getName())
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+    }
+
 }
