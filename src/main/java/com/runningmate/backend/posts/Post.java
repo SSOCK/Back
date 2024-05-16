45c9e8299261a97baeab040246334c474bc0f8a4
peageon <jinsoo2005@gmail.com>
2024-05-17 01:29:04 +0900

    feat: create Post entity

---
+++ src/main/java/com/runningmate/backend/posts/Post.java
@@ -0,0 +1,46 @@
+package com.runningmate.backend.posts;
+
+import com.runningmate.backend.member.Member;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Table(name = "post")
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    private LocalDateTime modifiedAt;
+
+    public void updatePost(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
