d9541139dcae9625e78e5fafb32985d5c41c3d7d
peageon <jinsoo2005@gmail.com>
2024-05-17 02:07:51 +0900

    feat: Handle MethodArgumentValidException globally in exception handler

--- src/main/java/com/runningmate/backend/GlobalExceptionHandler.java
+++ src/main/java/com/runningmate/backend/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.runningmate.backend;
 
-import com.runningmate.backend.member.exception.FieldExistsException;
+import com.runningmate.backend.exception.ErrorMessageResponseDTO;
+import com.runningmate.backend.exception.FieldExistsException;
+import com.runningmate.backend.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,7 +18,25 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(FieldExistsException.class)
-    public ResponseEntity<Map<String, List<String>>> handleFieldExistsException(FieldExistsException fe) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("exists", fe.getExists()));
+    public ResponseEntity<Map<String, List<String>>> handleFieldExistsException(FieldExistsException ee) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("exists", e.getExists()));
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorMessageResponseDTO> handleResourceNotFoundException(ResourceNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorMessageResponseDTO(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
