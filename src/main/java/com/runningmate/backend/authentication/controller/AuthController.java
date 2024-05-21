f20435cd4e42b575def50d8d58111888468c8652
송현우 <83938394+yunuo46@users.noreply.github.com>
2024-05-21 15:37:47 +0900

    feat: auth controller crossorigin 추가

--- src/main/java/com/runningmate/backend/authentication/controller/AuthController.java
+++ src/main/java/com/runningmate/backend/authentication/controller/AuthController.java
@@ -2,15 +2,13 @@ package com.runningmate.backend.authentication.controller;
 
 import com.runningmate.backend.authentication.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET})
 @RequestMapping(value = "/auth")
 @RequiredArgsConstructor
 public class AuthController {
