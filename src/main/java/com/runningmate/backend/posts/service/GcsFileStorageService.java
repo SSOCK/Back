e8966f3494f4e9ad0ba2878198094832831e722d
peageon <jinsoo2005@gmail.com>
2024-05-28 02:28:18 +0900

    feat: 5MB limit to image and make uploading image optional

--- src/main/java/com/runningmate/backend/posts/service/GcsFileStorageService.java
+++ src/main/java/com/runningmate/backend/posts/service/GcsFileStorageService.java
@@ -4,6 +4,7 @@ import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.WriteChannel;
 import com.google.cloud.storage.*;
+import com.runningmate.backend.exception.InvalidFileTypeException;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.ResourceUtils;
@@ -13,10 +14,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class GcsFileStorageService {
+    private static final List<String> ACCEPTED_CONTENT_TYPES = Arrays.asList("image/jpeg", "image/png", "image/gif");
 
     @Value("${spring.cloud.gcp.storage.credentials.location}")
     private String keyFileName;
@@ -25,6 +29,10 @@ public class GcsFileStorageService {
     private String bucketName;
 
     public String storeFile(MultipartFile file) throws RuntimeException, IOException {
+        if (!validateFile(file)) {
+            return null;
+        }
+
         InputStream keyFile = ResourceUtils.getURL(keyFileName).openStream();
 
         String fileName = UUID.randomUUID().toString();
@@ -43,5 +51,19 @@ public class GcsFileStorageService {
         } catch (IOException e) {
             throw new RuntimeException("Failed to store file in GCS", e); // throws 500
         }
+    }
+
+    private boolean validateFile(MultipartFile file) {
+        if (file == null || file.isEmpty()) {
+            return false;
+        }
+        if (!ACCEPTED_CONTENT_TYPES.contains(file.getContentType())) {
+            throw new InvalidFileTypeException(file.getContentType());
+        }
+        String filename = file.getOriginalFilename();
+        if (filename != null && !filename.toLowerCase().matches(".*\\.(jpg|jpeg|png|gif)$")) {
+            throw new InvalidFileTypeException(file.getContentType(), filename.toLowerCase());
+        }
+        return true;
     }
 }
