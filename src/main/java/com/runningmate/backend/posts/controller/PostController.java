494f5eeef416427e561a226e75fff79162e18a4e
peageon <jinsoo2005@gmail.com>
2024-05-28 01:51:25 +0900

    feat: Add image to post(maximum 1 at the moment)

--- src/main/java/com/runningmate/backend/posts/controller/PostController.java
+++ src/main/java/com/runningmate/backend/posts/controller/PostController.java
@@ -1,20 +1,21 @@
 package com.runningmate.backend.posts.controller;
 
 import com.runningmate.backend.member.Member;
-import com.runningmate.backend.member.dto.PostResponseDto;
+import com.runningmate.backend.posts.dto.PostResponseDto;
 import com.runningmate.backend.member.service.MemberService;
-import com.runningmate.backend.posts.Post;
 import com.runningmate.backend.posts.dto.CreatePostRequest;
+import com.runningmate.backend.posts.service.GcsFileStorageService;
 import com.runningmate.backend.posts.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -23,12 +24,14 @@ import java.util.List;
 public class PostController {
     private final PostService postService;
     private final MemberService memberService;
+    private final GcsFileStorageService gcsFileStorageService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    public PostResponseDto createNewPost(@Valid @RequestBody CreatePostRequest createPostRequest, Authentication authentication) {
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        return postService.createPost(createPostRequest, userDetails.getUsername());
+    public PostResponseDto createNewPost(@Valid @ModelAttribute CreatePostRequest createPostRequest, @AuthenticationPrincipal UserDetails userDetails) throws RuntimeException, IOException {
+        String username = userDetails.getUsername();
+        String imageUrl = gcsFileStorageService.storeFile(createPostRequest.getImage());
+        return postService.createPost(createPostRequest, username, imageUrl);
     }
 
     @ResponseStatus(HttpStatus.OK)
