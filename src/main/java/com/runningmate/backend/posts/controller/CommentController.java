268fa5d84ef8ac5270724d260bc073180ddc250c
peageon <jinsoo2005@gmail.com>
2024-06-05 03:45:29 +0900

    feat: created api endpoint for deleting comment

--- src/main/java/com/runningmate/backend/posts/controller/CommentController.java
+++ src/main/java/com/runningmate/backend/posts/controller/CommentController.java
@@ -1,11 +1,23 @@
 package com.runningmate.backend.posts.controller;
 
+import com.runningmate.backend.posts.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/comments")
 public class CommentController {
+    private final CommentService commentService;
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{commentId}")
+    public void deleteComment(@PathVariable(name = "commentId") Long commentId,
+                           @AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        commentService.deleteComment(commentId, username);
+    }
 }
