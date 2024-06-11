d2a0336b0e6636b9cddb3f72084f1d176bdf74dd
peageon <jinsoo2005@gmail.com>
2024-06-12 01:27:42 +0900

    feat: User can add a new course

---
+++ src/main/java/com/runningmate/backend/route/dto/CoordinateDto.java
@@ -0,0 +1,13 @@
+package com.runningmate.backend.route.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CoordinateDto {
+    private double latitude;
+    private double longitude;
+}
---
+++ src/main/java/com/runningmate/backend/route/repository/RouteRepository.java
@@ -0,0 +1,7 @@
+package com.runningmate.backend.route.repository;
+
+import com.runningmate.backend.route.Route;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RouteRepository extends JpaRepository<Route, Long> {
+}
