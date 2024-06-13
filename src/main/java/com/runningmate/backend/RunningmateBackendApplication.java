c88f673d5fbb4e37870023bd2d8269ff9fe1a202
peageon <jinsoo2005@gmail.com>
2024-06-13 17:13:09 +0900

    fix: Change server timezone to utc

--- src/main/java/com/runningmate/backend/RunningmateBackendApplication.java
+++ src/main/java/com/runningmate/backend/RunningmateBackendApplication.java
@@ -1,14 +1,23 @@
 package com.runningmate.backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication()
+@EnableJpaAuditing
 public class RunningmateBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RunningmateBackendApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 }
