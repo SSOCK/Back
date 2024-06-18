84bd4572c9627818bc6121de20dfc74d2a53abbf
peageon <jinsoo2005@gmail.com>
2024-06-18 16:53:04 +0900

    feat: api endpoint to get nearby courses within radius

--- src/main/java/com/runningmate/backend/route/controller/RouteController.java
+++ src/main/java/com/runningmate/backend/route/controller/RouteController.java
@@ -1,15 +1,20 @@
 package com.runningmate.backend.route.controller;
 
+import com.runningmate.backend.route.dto.RouteListResponseDto;
 import com.runningmate.backend.route.dto.RouteRequestDto;
 import com.runningmate.backend.route.dto.RouteResponseDto;
 import com.runningmate.backend.route.service.RouteService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/courses")
@@ -29,5 +34,15 @@ public class RouteController {
     public RouteResponseDto getRouteById(@PathVariable(name = "routeId") Long routeId, @AuthenticationPrincipal UserDetails userDetails) {
         RouteResponseDto routeResponseDto = routeService.getRouteById(routeId);
         return routeResponseDto;
+    }
+
+    @GetMapping("/radius")
+    @ResponseStatus(HttpStatus.OK)
+    public RouteListResponseDto getCourseWithinRadius(@RequestParam(name = "latitude") double latitude,
+                                                        @RequestParam(name = "longitude") double longitude,
+                                                        @RequestParam(name = "radius") @Max(5000) @Min(0) int radius) {
+        List<RouteResponseDto> routeResponseDtoList = routeService.getRoutesWithinRadius(latitude, longitude, radius);
+
+        return new RouteListResponseDto(routeResponseDtoList);
     }
 }
