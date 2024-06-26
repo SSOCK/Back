0309f116c7b0d9f1a3a1609ccc6521741a020c8f
peageon <jinsoo2005@gmail.com>
2024-06-27 02:55:27 +0900

    feat: add RouteSaveList and RouteSaveListMemberRoute and modify MemberRoute to save lists of saved routes

--- src/main/java/com/runningmate/backend/member/repository/MemberRouteRepository.java
+++ src/main/java/com/runningmate/backend/member/repository/MemberRouteRepository.java
@@ -2,6 +2,7 @@ package com.runningmate.backend.member.repository;
 
 import com.runningmate.backend.member.Member;
 import com.runningmate.backend.member.MemberRoute;
+import com.runningmate.backend.route.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +11,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface MemberRouteRepository extends JpaRepository<MemberRoute, UUID> {
-    List<MemberRoute> findByMemberAndSavedTrue(Member member);
-    List<MemberRoute> findByMemberAndLikedTrue(Member member);
+public interface MemberRouteRepository extends JpaRepository<MemberRoute, Long> {
     Optional<MemberRoute> findByMemberIdAndRouteId(Long memberId, Long routeId);
+
+    Optional<MemberRoute> findByMemberAndRoute(Member member, Route route);
 }
