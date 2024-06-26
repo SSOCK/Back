f6bf947134cca4570256ab66beb35be555eb48d3
peageon <jinsoo2005@gmail.com>
2024-06-27 02:55:54 +0900

    feat: modify NoPermissionException to take in message

--- src/main/java/com/runningmate/backend/exception/NoPermissionException.java
+++ src/main/java/com/runningmate/backend/exception/NoPermissionException.java
@@ -4,4 +4,8 @@ public class NoPermissionException extends RuntimeException{
     public NoPermissionException() {
         super("User does not have permission to perform this command");
     }
+
+    public NoPermissionException(String message) {
+        super(message);
+    }
 }
