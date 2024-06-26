dc5c66c26168cff0820f43f70ca67eda06e2bd13
peageon <jinsoo2005@gmail.com>
2024-06-27 03:13:42 +0900

    feat: create request and response dtos for RouteSaveList

---
+++ src/main/java/com/runningmate/backend/route/dto/RouteSaveListResponseDto.java
@@ -0,0 +1,40 @@
+package com.runningmate.backend.route.dto;
+
+import com.runningmate.backend.member.dto.MemberRouteDto;
+import com.runningmate.backend.route.RouteSaveList;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+public class RouteSaveListResponseDto {
+    private Long id;
+    private String name;
+    private String memberUsername;
+
+    private int routeCount;
+    private List<MemberRouteDto> memberRoutes;
+
+
+    public RouteSaveListResponseDto(RouteSaveList routeSaveList) {
+        this.id = routeSaveList.getId();
+        this.name = routeSaveList.getName();
+        this.memberUsername = routeSaveList.getMember().getUsername();
+        this.routeCount = routeSaveList.getRouteSaveListMemberRoutes().size();
+    }
+
+    public RouteSaveListResponseDto(RouteSaveList routeSaveList, boolean includeMemberRoutes) {
+        this.id = routeSaveList.getId();
+        this.name = routeSaveList.getName();
+        this.memberUsername = routeSaveList.getMember().getUsername();
+        this.routeCount = routeSaveList.getRouteSaveListMemberRoutes().size();
+        if (includeMemberRoutes) {
+            this.memberRoutes = routeSaveList.getRouteSaveListMemberRoutes().stream()
+                    .map(routeSaveListMemberRoute -> new MemberRouteDto(routeSaveListMemberRoute.getMemberRoute()))
+                    .collect(Collectors.toList());
+        }
+    }
+}
