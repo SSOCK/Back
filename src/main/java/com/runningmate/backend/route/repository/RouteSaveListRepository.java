665ff8bc4daa8882c0cc956ef2667c0803c2a8d8
peageon <jinsoo2005@gmail.com>
2024-06-27 13:23:00 +0900

    feat: returns 409 status code when user tries to create RouteSaveList with same name as existing one

--- src/main/java/com/runningmate/backend/route/repository/RouteSaveListRepository.java
+++ src/main/java/com/runningmate/backend/route/repository/RouteSaveListRepository.java
@@ -5,7 +5,9 @@ import com.runningmate.backend.route.RouteSaveList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RouteSaveListRepository extends JpaRepository<RouteSaveList, Long> {
     List<RouteSaveList> findByMember(Member member);
+    boolean existsByNameAndMember(String name, Member member);
 }
