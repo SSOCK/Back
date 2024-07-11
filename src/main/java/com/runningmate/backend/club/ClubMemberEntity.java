9c58ced55f2a1296a9464bd21ee1a3da4b76b58c
peageon <jinsoo2005@gmail.com>
2024-07-12 00:20:42 +0900

    feat: created entities for club page

---
+++ src/main/java/com/runningmate/backend/club/ClubMemberEntity.java
@@ -0,0 +1,33 @@
+package com.runningmate.backend.club;
+
+import com.runningmate.backend.member.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "club_name")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ClubMemberEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_id")
+    private Club club;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private ClubRole role = ClubRole.MEMBER;
+
+
+
+}
---
+++ src/main/java/com/runningmate/backend/club/ClubScheduleEntity.java
@@ -0,0 +1,27 @@
+package com.runningmate.backend.club;
+
+import com.runningmate.backend.member.Member;
+import com.runningmate.backend.schedule.Schedule;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@Table(name = "club_schedule")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ClubScheduleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "club_schedule_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_id")
+    private Club club;
+}
