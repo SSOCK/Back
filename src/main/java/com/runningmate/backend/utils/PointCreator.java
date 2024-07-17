d32e142a7e7230b7e71fd49a43240a01bd810100
peageon <jinsoo2005@gmail.com>
2024-07-18 03:51:20 +0900

    feat: utility class to create Point object from latitude and longitude and the other way around

---
+++ src/main/java/com/runningmate/backend/utils/PointCreator.java
@@ -0,0 +1,22 @@
+package com.runningmate.backend.utils;
+
+import com.runningmate.backend.route.dto.CoordinateDto;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+
+public class PointCreator {
+
+    public static Point createPoint(double longitude, double latitude) {
+        GeometryFactory geometryFactory = new GeometryFactory();
+        Coordinate coordinate = new Coordinate(longitude, latitude);
+        return geometryFactory.createPoint(coordinate);
+    }
+
+    public static CoordinateDto toCoordinateDto(Point point) {
+        if (point == null) {
+            throw new IllegalArgumentException("Point cannot be null");
+        }
+        return new CoordinateDto(point.getY(), point.getX());
+    }
+}
