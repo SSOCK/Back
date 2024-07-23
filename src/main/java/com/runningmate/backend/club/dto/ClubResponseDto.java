a935cecde48cf908200557d6d717b1af7741dd52
peageon <jinsoo2005@gmail.com>
2024-07-24 01:06:47 +0900

    feat: instead of just returning clubMembers, sort them in clubOwners, clubModerators, clubMembers

--- src/main/java/com/runningmate/backend/club/dto/ClubResponseDto.java
+++ src/main/java/com/runningmate/backend/club/dto/ClubResponseDto.java
@@ -3,6 +3,7 @@ package com.runningmate.backend.club.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.runningmate.backend.club.Club;
 import com.runningmate.backend.club.ClubMemberEntity;
+import com.runningmate.backend.club.ClubRole;
 import com.runningmate.backend.member.dto.MemberDto;
 import com.runningmate.backend.route.dto.CoordinateDto;
 import com.runningmate.backend.schedule.Schedule;
@@ -11,6 +12,7 @@ import lombok.*;
 import org.locationtech.jts.geom.Point;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -27,10 +29,22 @@ public class ClubResponseDto {
     private CoordinateDto locationCoordinate;
     private String profilePic;
     private String backgroundPic;
+    private List<MemberDto> clubOwners;
+    private List<MemberDto> clubModerators;
     private List<MemberDto> clubMembers;
     private List<ClubScheduleEntityResponseDto> schedules;
 
     public static ClubResponseDto fromEntity(Club club) {
+        Map<ClubRole, List<MemberDto>> groupedMembers = club.getMembers().stream()
+                .collect(Collectors.groupingBy(
+                        ClubMemberEntity::getRole,
+                        Collectors.mapping(clubMember -> MemberDto.fromEntity(clubMember.getMember()), Collectors.toList())
+                ));
+
+        List<MemberDto> owners = groupedMembers.getOrDefault(ClubRole.OWNER, List.of());
+        List<MemberDto> moderators = groupedMembers.getOrDefault(ClubRole.MODERATOR, List.of());
+        List<MemberDto> members = groupedMembers.getOrDefault(ClubRole.MEMBER, List.of());
+
         return ClubResponseDto.builder()
                 .id(club.getId())
                 .title(club.getTitle())
@@ -39,10 +53,9 @@ public class ClubResponseDto {
                 .locationCoordinate(PointCreator.toCoordinateDto(club.getLocationCoordinate()))
                 .profilePic(club.getProfile_pic())
                 .backgroundPic(club.getBackground_pic())
-                .clubMembers(club.getMembers().stream()
-                        .map(ClubMemberEntity::getMember)
-                        .map(MemberDto::fromEntity)
-                        .collect(Collectors.toList()))
+                .clubOwners(owners)
+                .clubModerators(moderators)
+                .clubMembers(members)
                 .schedules(club.getSchedules().stream()
                         .map(clubScheduleEntity -> ClubScheduleEntityResponseDto.fromEntity(clubScheduleEntity))
                         .collect(Collectors.toList()))
