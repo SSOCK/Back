794544e78045df1be15b4ea8e984292a41a02ea2
peageon <jinsoo2005@gmail.com>
2024-07-24 00:46:40 +0900

    fix: forgot query parameter to find activities by member

--- src/main/java/com/runningmate/backend/activity/repository/ActivityRepository.java
+++ src/main/java/com/runningmate/backend/activity/repository/ActivityRepository.java
@@ -1,10 +1,11 @@
 package com.runningmate.backend.activity.repository;
 
 import com.runningmate.backend.activity.Activity;
+import com.runningmate.backend.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
-    List<Activity> findAllByMemberOrderByCreatedAtDesc();
+    List<Activity> findAllByMemberOrderByCreatedAtDesc(Member member);
 }
